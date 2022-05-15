32b35578faa514abe3b27db3d98e9a0611e24a20
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-05-15 14:38:50 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,c=0,s;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=arr[i];         if(s%2==0)         {             c++;         }     }     if(c==n)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Even_Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,c=0,s;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=arr[i];
+        if(s%2==0)
+        {
+            c++;
+        }
+    }
+    if(c==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
