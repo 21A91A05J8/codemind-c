3bd57a732590f78fb384f24030655e00cff7acc4
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-01 09:59:33 +0530

    #include<stdio.h> int main() {     int m,n,i,s,dc=0,c=0,r,re;     scanf("%d",&m);     scanf("%d",&n);     for(i=m;i<=n;i++)     {        s=i;        dc=0;        c=0;        while(s!=0)        {            r=s%10;            dc++;            s=s/10;        }        s=i;        while(s!=0)        {            r=s%10;            if(r==0)            {                 s=s/10;                 continue;            }            re=i%r;            if(re==0)            {                c++;            }            s=s/10;        }        if(c==dc)        {            printf("%d ",i);        }     }     return 0; }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,40 +1,40 @@
 #include<stdio.h>
 int main()
 {
-    int m,n,i,r,dc=0,re,c=0,s;
+    int m,n,i,s,dc=0,c=0,r,re;
     scanf("%d",&m);
     scanf("%d",&n);
     for(i=m;i<=n;i++)
     {
-        dc=0;
-        c=0;
-        s=i;
-        while(s!=0)
-        {
-            r=s%10;
-            dc++;
-            s=s/10;
-        }
-        s=i;
-        while(s!=0)
-        {
-            r=s%10;
-            if(r==0)
-            {
+       s=i;
+       dc=0;
+       c=0;
+       while(s!=0)
+       {
+           r=s%10;
+           dc++;
+           s=s/10;
+       }
+       s=i;
+       while(s!=0)
+       {
+           r=s%10;
+           if(r==0)
+           {
                 s=s/10;
                 continue;
-            }
-            re=i%r;
-            if(re==0)
-            {
-                c++;
-            }
-            s=s/10;
-        }
-        if(c==dc)
-        {
-           printf("%d ",i); 
-        }
-     }
-     return 0;
+           }
+           re=i%r;
+           if(re==0)
+           {
+               c++;
+           }
+           s=s/10;
+       }
+       if(c==dc)
+       {
+           printf("%d ",i);
+       }
+    }
+    return 0;
 }
