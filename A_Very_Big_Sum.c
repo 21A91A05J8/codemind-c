2aee173d9337900ff0f42902399fd79d17757065
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-09 14:23:40 +0530

    #include<stdio.h> int main() {     int n,i;     long int arr[10],sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%ld",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     printf("%ld",sum);     return 0; }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    long int arr[10],sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    printf("%ld",sum);
+    return 0;
+}
