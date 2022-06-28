e850485e1d45f60e8b06166343dfaef138d9b921
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-28 21:54:19 +0530

    #include<stdio.h> int main() {     int n,f=0;     scanf("%d",&n);     if(n%3==0)     {         printf("Pling");         f=1;     }     if(n%5==0)     {         printf("Plang");         f=1;     }     if(n%7==0)     {         printf("Plong");         f=1;     }     if(f==0)     {         printf("%d",n);     } }

---
+++ RainDrop.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,f=0;
+    scanf("%d",&n);
+    if(n%3==0)
+    {
+        printf("Pling");
+        f=1;
+    }
+    if(n%5==0)
+    {
+        printf("Plang");
+        f=1;
+    }
+    if(n%7==0)
+    {
+        printf("Plong");
+        f=1;
+    }
+    if(f==0)
+    {
+        printf("%d",n);
+    }
+}
