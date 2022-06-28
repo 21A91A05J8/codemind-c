9f1a4b80da8689a6904bb473b754197f4c308862
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-28 18:59:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,r,sum=0;     scanf("%d",&n);     while(n!=0)     {         while(n!=0)         {             r=n%10;             sum=sum+pow(r,2);             n=n/10;         }         if(sum<10)         {             if(sum==1||sum==7)             {                 printf("True");             }             else             {                 printf("False");             }         }         else         {             n=sum;             sum=0;             continue;         }     }     return 0; }

--- happy_number.c
+++ happy_number.c
@@ -1,26 +1,34 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int n,r,sum=0;
     scanf("%d",&n);
-    while(sum!=1&&sum!=4)
+    while(n!=0)
     {
-        sum=0;
         while(n!=0)
         {
             r=n%10;
-            sum+=(r*r);
-            n/=10;
+            sum=sum+pow(r,2);
+            n=n/10;
         }
-        n=sum;
-    }
-    if(sum==1)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
+        if(sum<10)
+        {
+            if(sum==1||sum==7)
+            {
+                printf("True");
+            }
+            else
+            {
+                printf("False");
+            }
+        }
+        else
+        {
+            n=sum;
+            sum=0;
+            continue;
+        }
     }
     return 0;
 }
