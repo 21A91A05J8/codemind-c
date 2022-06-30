e9227708a953a3370eb75b063fb02133c42b2159
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-30 21:32:20 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j,c=0,f=0,max=0,min=99999;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=1;         if(a[i]!=-1)         {             for(j=0;j<n;j++)             {                 if(a[i]==a[j]&&i!=j)                 {                     c++;                     a[j]=-1;                 }             }             if(a[i]==c)             {                 if(a[i]>max)                 {                     max=a[i];                 }                 if(a[i]<min)                 {                     min=a[i];                 }                 f=1;             }         }     }     if(f==0)     {         printf("-1");     }     else     {         printf("%d %d",min,max);     } }

---
+++ Maximum_and_Minimum.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j,c=0,f=0,max=0,min=99999;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        if(a[i]!=-1)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i]==a[j]&&i!=j)
+                {
+                    c++;
+                    a[j]=-1;
+                }
+            }
+            if(a[i]==c)
+            {
+                if(a[i]>max)
+                {
+                    max=a[i];
+                }
+                if(a[i]<min)
+                {
+                    min=a[i];
+                }
+                f=1;
+            }
+        }
+    }
+    if(f==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d %d",min,max);
+    }
+}
