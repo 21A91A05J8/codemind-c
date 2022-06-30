ff5ab094193f298181755b9dc9b99a57b7fd5a52
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-30 22:50:44 +0530

    #include<stdio.h> int main() {     int n,a[100],i,c,b,x=0,y=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&c,&b);     for(i=0;i<n;i++)     {         if(c<=a[i]&&b>=a[i]||c>=a[i]&&b<=a[i])         {             if(x<a[i])             {                 x=a[i];                 y=1;             }         }     }     if(y==1)     {         printf("%d",x);     }     else     {         printf("-1");     } }

---
+++ maximum_of_numbers_between_A_and_B.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,c,b,x=0,y=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&c,&b);
+    for(i=0;i<n;i++)
+    {
+        if(c<=a[i]&&b>=a[i]||c>=a[i]&&b<=a[i])
+        {
+            if(x<a[i])
+            {
+                x=a[i];
+                y=1;
+            }
+        }
+    }
+    if(y==1)
+    {
+        printf("%d",x);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
