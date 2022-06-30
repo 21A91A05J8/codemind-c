b6a7876bdc6c26da1db1456c4b1e101d83d31b49
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-30 23:05:11 +0530

    #include<stdio.h> int main() {     int n,i,j,a[100],min=999999,p,q,f=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&p,&q);     for(i=0;i<n;i++)     {         if(a[i]<p||a[i]>q)         {             if(min>a[i])             {                min=a[i];                f=1;              }         }     }     if(f==1)     {         printf("%d",min);     }     else     {         printf("-1");     } }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a[100],min=999999,p,q,f=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&p,&q);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<p||a[i]>q)
+        {
+            if(min>a[i])
+            {
+               min=a[i];
+               f=1;
+             }
+        }
+    }
+    if(f==1)
+    {
+        printf("%d",min);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
