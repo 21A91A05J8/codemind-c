975baecbfe9feb8f03a198fdaa19c0ae500196b8
21A91A05J8 <97426310+21A91A05J8@users.noreply.github.com>
2022-06-30 23:17:11 +0530

    #include<stdio.h> int main() {     int n,a[100],i,x=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<=n;i++)     {         if(a[i]%2==1)         {             break;         }         x+=a[i];     }     printf("%d",x); }

---
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,x=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            break;
+        }
+        x+=a[i];
+    }
+    printf("%d",x);
+}
